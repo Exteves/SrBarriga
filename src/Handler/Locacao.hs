ddb9be07693294a8e14ce1bfac46131a808e0ba4
Alyne Bartolotto <alynecbb@outlook.com>
2017-12-06 21:53:21 +0000

    26 - Mensagens de retorno alteradas em Locacao

--- src/Handler/Locacao.hs
+++ src/Handler/Locacao.hs
@@ -14,32 +14,34 @@ postLocacaoInsereR = do
     locacao <- requireJsonBody :: Handler Locacao
     liftIO $ print locacao
     loc <- runDB $ insert locacao
-    sendStatusJSON created201 (object ["resp" .= (fromSqlKey loc)])
+    sendStatusJSON created201 (object ["Inserção feita no Id" .= (fromSqlKey loc)])
 
 getLocacaoCarroBuscarR :: CarroId -> Handler Value
-getLocacaoCarroBuscarR = undefined
+getLocacaoCarroBuscarR carid = do
+    carro <- runDB $ get404 carid
+    sendStatusJSON ok200 (object ["Busca pelo Id do Carro" .= (toJSON carro)])
 
 getLocacaoClienteBuscarR :: ClienteId -> Handler Value
 getLocacaoClienteBuscarR cid = do
     cliente <- runDB $ get404 cid
-    sendStatusJSON ok200 (object ["resp" .= (toJSON cliente)])
+    sendStatusJSON ok200 (object ["Busca pelo Id do Cliente" .= (toJSON cliente)])
 
 getLocacaoCartaoBuscarR :: CartaoId -> Handler Value
 getLocacaoCartaoBuscarR cardid = do
     cartao <- runDB $ get404 cardid
-    sendStatusJSON ok200 (object ["resp" .= (toJSON cartao)])
+    sendStatusJSON ok200 (object ["Busca pelo Id do Cartao" .= (toJSON cartao)])
 
 getLocacaoCorretorBuscarR :: CorretorId -> Handler Value
 getLocacaoCorretorBuscarR corid = do
     corretor <- runDB $ get404 corid
-    sendStatusJSON ok200 (object ["resp" .= (toJSON corretor)])
+    sendStatusJSON ok200 (object ["Busca pelo Id do Corretor" .= (toJSON corretor)])
 
 getLocacaoRetornoBuscarR :: Day -> Handler Value
 getLocacaoRetornoBuscarR retorno = do
     retList <- runDB $ selectFirst [LocacaoDataRetorno==.retorno][]
-    sendStatusJSON ok200(object ["resp".= (toJSON retList)])
+    sendStatusJSON ok200(object ["Busca por data de Retorno".= (toJSON retList)])
     
 getLocacaoSaidaBuscarR :: Day -> Handler Value
 getLocacaoSaidaBuscarR saida = do
     saidaList <- runDB $ selectFirst [LocacaoDataSaida==.saida][]
-    sendStatusJSON ok200(object ["resp".= (toJSON saidaList)])
+    sendStatusJSON ok200(object ["Busca por data de Saida".= (toJSON saidaList)])
