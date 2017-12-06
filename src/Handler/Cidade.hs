db4b91c6db8c3ce8564e4475ee809087190c368a
Alyne Bartolotto <alynecbb@outlook.com>
2017-12-06 21:57:48 +0000

    29 - Handler Cidade criado

---
+++ src/Handler/Cidade.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+module Handler.Cidade where
+
+import Import
+import Network.HTTP.Types.Status
+import Database.Persist.Postgresql
+
+
+postCidadeInsereR :: Handler Value
+postCidadeInsereR = do
+    cidade <- requireJsonBody :: Handler Cidade
+    cidid <- runDB $ insert cidade
+    sendStatusJSON created201 (object ["resp" .= (fromSqlKey cidid)])
+
+{--getCidadeBuscarR :: EnderecoId -> Handler Value
+getCidadeBuscarR cidid = do
+    cidade <- runDB $ get404 cidid
+    sendStatusJSON ok200 (object ["resp" .= (toJSON cidade)])
+    --}
